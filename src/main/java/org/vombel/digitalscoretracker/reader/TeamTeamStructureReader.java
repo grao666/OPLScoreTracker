bdda903f8a878ec1374c6d9bd5521bb68324f778
Ganesha Sunder Rao Vombathkere <ganeshas@oath.com>
2018-09-04 00:39:16 +0530

    first commit

---
+++ src/main/java/org/vombel/digitalscoretracker/reader/TeamTeamStructureReader.java
@@ -0,0 +1,10 @@
+package org.vombel.digitalscoretracker.reader;
+
+public class TeamTeamStructureReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
